732a444461d0eebcdf1a0d51b9d839e4e7734cb7
Vignesh.Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-03-04 08:08:22 +0100

    certificate and import changes

--- host/example.py
+++ host/example.py
@@ -1,4 +1,4 @@
-from hololinked.server import create_primary_host
+from hololinked.system_host import create_system_host
 from pathlib import Path
 import os
 from tornado import ioloop
@@ -9,8 +9,8 @@ ssl_context.load_cert_chain(
     str(Path(os.path.dirname(__file__)).parent) + '\\assets\\security\\certificate.pem',
     keyfile = str(Path(os.path.dirname(__file__)).parent) + '\\assets\\security\\key.pem')
 
-server = create_primary_host(str(Path(os.path.dirname(__file__)).parent) + "\\assets\\db_config.json", 
-                            ssl_context=ssl_context)
+server = create_system_host(str(Path(os.path.dirname(__file__)).parent) + "\\assets\\db_config.json", 
+                            ssl_context=ssl_context, CORS=["https://localhost:5173"])
 server.listen(8080)
 event_loop = ioloop.IOLoop.current()
 print("starting server")
