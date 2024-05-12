a89f5c27655aff9740a0e1a77e9d3f163f963dc7
Vignesh.Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2024-05-12 11:29:14 +0200

    removed host examples &  updated gitignore

--- host/delete_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy_utils import drop_database
-from hololinked.server.database import BaseDB
-from pathlib import Path
-import os
-
-config_file = str(Path(os.path.dirname(__file__)).parent) + "\\assets\\db_config.json"
-URL = BaseDB.create_postgres_URL(config_file, database="hololinked-host", use_dialect=False)
-drop_database(URL)
-print(f"deleted DB with URL {URL}")
--- host/subscription.py
+++
@@ -1,4 +0,0 @@
-from hololinked.server import HTTPServer
-
-H = HTTPServer([], port=8082, host="https://localhost:8081")
-H.listen()
